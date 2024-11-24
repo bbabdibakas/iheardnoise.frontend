8780ee4c8b6a27d6658d0fc0a8c2013ef341110d
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-11-24 21:24:23 +0500

    feat: added components AppLink, Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Link, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import MainPage from "./pages/MainPage"
 import ProfilePage from "./pages/ProfilePage"
+import Sidebar from "./components/Sidebar/Sidebar"
 
 const App = () => {
     return (
         <div className='wrapper'>
-            <Link to="/">Main</Link>
-            <Link to="/profile">Profile</Link>
+            <Sidebar />
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/profile" element={<ProfilePage />} />
---
+++ src/components/AppLink/AppLink.tsx
@@ -0,0 +1,25 @@
+import { Link, LinkProps } from 'react-router-dom'
+import cls from './AppLink.module.scss'
+
+interface AppLinkProps extends LinkProps {
+    className?: string
+}
+
+const AppLink = (props: AppLinkProps) => {
+    const {
+        to,
+        children,
+        className
+    } = props
+
+    return (
+        <Link
+            to={to}
+            className={`${cls.AppLink} ${className}`}
+        >
+            {children}
+        </Link>
+    )
+}
+
+export default AppLink

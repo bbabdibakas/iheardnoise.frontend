0e70118aff08f1036280ee41e86712e3593b3e48
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-11-28 20:29:47 +0500

    feat: added ui kit AppPortal, AppModal

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,18 +1,29 @@
-import AppButton from "shared/ui/AppButton/AppButton"
+import { useState } from "react"
+import AppButton, { AppButtonTheme } from "shared/ui/AppButton/AppButton"
 import AppHeader from "shared/ui/AppHeader/AppHeader"
+import { AppModal } from "shared/ui/AppModal/AppModal"
 
 const MainPage = () => {
-    const onClickButton = () => {
-        alert('Clicked!')
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+
+    const onModalOpen = () => {
+        setIsModalOpen(true)
+    }
+
+    const onModalClose = () => {
+        setIsModalOpen(false)
     }
 
     return (
         <div>
             <AppHeader>
-                <AppButton onClick={onClickButton}>
+                <AppButton onClick={onModalOpen} theme={AppButtonTheme.PRIMARY}>
                     Login
                 </AppButton>
             </AppHeader>
+            <AppModal isOpen={isModalOpen} onClose={onModalClose}>
+                Hello, World!
+            </AppModal>
             MainPage
         </div>
     )

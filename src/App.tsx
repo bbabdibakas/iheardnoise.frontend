9fdf3ba9b6d655d00a49c5e22de2679700d72997
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-12-08 20:24:23 +0500

    feat: added components AppLink, Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Route, Routes } from "react-router-dom"
-import { Link } from "react-router-dom"
 import { Suspense } from "react"
 import { MainPageAsync } from "./pages/MainPage.async"
 import { ProfilePageAsync } from "./pages/ProfilePage.async"
+import Sidebar from "./components/Sidebar/Sidebar"
 
 const App = () => {
     return (
         <div className="wrapper">
-            <Link to={'/'}>main</Link>
-            <Link to={'/profile'}>profile</Link>
+            <Sidebar />
             <Suspense fallback={'Loading...'}>
                 <Routes>
                     <Route path="/" element={<MainPageAsync />} />
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import AppLink from '../AppLink/AppLink'
+import cls from './Sidebar.module.scss'
+
+const Sidebar = () => {
+    return (
+        <div className={cls.Sidebar}>
+            <AppLink to="/" className={cls.link}>Main</AppLink>
+            <AppLink to="/profile" className={cls.link}>Profile</AppLink>
+        </div>
+    )
+}
+
+export default Sidebar

e0c3334e4f7457afda033258a67d6562ff540fb1
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-12-20 21:30:23 +0500

    feat: installed html-webpack-plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config: webpack.Configuration = {
     mode: 'development',
@@ -22,6 +23,11 @@ const config: webpack.Configuration = {
         clean: true
     },
     devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ]
 };
 
 export default config;
